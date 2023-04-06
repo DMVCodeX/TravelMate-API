8de8301aeb43c46b29bdfa88810da63dbbf6470b
DIANELLY VENTURA <DMVCode@outlook.com>
2023-04-05 23:20:27 -0400

    add index, show, create, update delete controller methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    render :show
+  end
+
+  def create
+    @user = User.create(
+      name: params[:name].downcase,
+      email: params[:email].downcase,
+      image: params[:image],
+      # password: params[:password],
+      # password_confirmation: params[:password_confirmation],
+    )
+
+    if @user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    @user.update(
+      name: params[:name] || @user.name,
+      email: params[:email] || @user.email,
+      image: params[:image] || @user.image,
+    )
+
+    render :show
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+    @user.destroy
+
+    render json: { message: "User sucessfully deleted" }
+  end
 end
