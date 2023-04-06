3268ab7d070efe3a708bd10dbfdeb7f919f0650f
DIANELLY VENTURA <DMVCode@outlook.com>
2023-04-05 23:36:23 -0400

    add sessions routes and controller create method

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(
-  name: "Diane",
-  email: "diane@test.com",
-  image: "https://avatars.githubusercontent.com/u/122197490?v=4",
-)
+# User.create(
+#   name: "Diane",
+#   email: "diane@test.com",
+#   image: "https://avatars.githubusercontent.com/u/122197490?v=4",
+# )
