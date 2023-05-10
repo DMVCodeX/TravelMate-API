f7637add24998d183ceeee1c8fcb26fbb9381d77
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-10 00:55:20 -0400

    add trip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_002840) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_045315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.string "user_id"
+    t.string "title"
+    t.string "image"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
